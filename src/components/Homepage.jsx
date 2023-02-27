a0e8e869364f39997181aff2944d4853615ebe73
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-02-27 18:13:50 +0300

    chore: second commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,8 +3,11 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 const { Title } = Typography
+import {useGetCryptosQuery} from "../services/cryptoApi";
 
 const Homepage = () => {
+    const {data, isFetching} = useGetCryptosQuery();
+    console.log(data)
     return (
         <>
             <Title level={2} className='heading'>Global Crypto Stats</Title>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,3 +1,27 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': 'f5d592a778msh9e67b6bf429cad3p150cd1jsn297d7a82ced0',
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
+
+const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins') //https://youtu.be/9DDX3US3kss?t=2963
+        })
+    })
+})
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+
 // const options = {
 //     method: 'GET',
 //     url: 'https://coinranking1.p.rapidapi.com/exchanges',
@@ -13,3 +37,4 @@
 //         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 //     }
 // };
+
