d909da379f43307c0f47312659dbb66dcddcf331
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-02-27 17:21:33 +0300

    chore: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route, Link, Routes, Navigate} from "react-router-dom";
+import {Layout, Typography, Space} from "antd";
+import {Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News} from "./components";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='app'>
+            <div className='navbar'>
+                <Navbar/>
+            </div>
+            <div className='main'>
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route index path='/' element={<Homepage/>}/>
+                            <Route path='/exchanges' element={<Exchanges/>}/>
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies/>}/>
+                            <Route path='/crypto/:coinId' element={<CryptoDetails/>}/>
+                            <Route path='/news' element={<News/>}/>
+                            <Route path='*' element={<Navigate to='/'/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className='footer'>
+                    <Typography.Title level={5} style={{color: "white", textAlign: "center",}}>
+                        Crypto Exchange <br/>
+                        All rights reserved &copy; RUS29TAM
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,15 @@
+// const options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//         referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//         limit: '50',
+//         offset: '0',
+//         orderBy: '24hVolume',
+//         orderDirection: 'desc'
+//     },
+//     headers: {
+//         'X-RapidAPI-Key': 'f5d592a778msh9e67b6bf429cad3p150cd1jsn297d7a82ced0',
+//         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+//     }
+// };
